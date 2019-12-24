c37da925d3046b20c11b2f18ef10e1045e353719
Youssef M Kassabi <youssef@devmyk.com>
2019-12-24 16:21:46 +0100

    added the fetch repos featur

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     return (
     <div className='container'>
       <Popular />
+      <hr/>
       <Hello 
         name={"MYK"}
         authed
--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from "prop-types";
+// import {string, func} from 'prop-types';
+import {
+    fetchPopularRepos
+} from '../utils/api';
+
+
 
 function NavBar ({selected, onUpdateLanguague}){
 
-const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Python'];
+const languages = ['All', 'JavaScript', 'Ruby', 'Python', 'Go', 'Dart'];
 
     return (
 
@@ -28,29 +35,71 @@ export default class Popular extends React.Component{
         super(props)
 
         this.state = {
-            currentLanguage: 'All'
+            currentLanguage: 'All',
+            repos: null,
+            errorMsg: null
         }
 
         this.updateLanguageNavBar = this.updateLanguageNavBar.bind(this)
+        this.isLoading = this.isLoading.bind(this)
     }
 
     updateLanguageNavBar(currentLanguage) {
         this.setState({
-            currentLanguage
+            currentLanguage,
+            errorMsg: null,
+            repos: null
+        })
+        fetchPopularRepos(currentLanguage)
+        .then(repos => this.setState({
+            repos, 
+            errorMsg:null
+            })
+        )
+        .catch(()=>{
+            this.setState({errorMsg: 'Sorry, Failed to fetch Repos!'})
         })
     }
 
+
+isLoading() {
+    return this.state.repos === null && this.state.error === null 
+}
+
+
+componentDidMount(){
+    this.updateLanguageNavBar(this.setState.currentLanguage)
+}
+
+
     render(){
         
-        const {currentLanguage} = this.state
+        const {currentLanguage,repos, errorMsg} = this.state
 
         return(
             <React.Fragment>
                 <NavBar 
                     selected={currentLanguage}
                     onUpdateLanguague ={this.updateLanguageNavBar}
-                />
+            />
+
+                {this.isLoading() && <h2> LOADING, Patience please ....</h2>}   
+                {errorMsg && {errorMsg}}
+
+                 {
+                     repos && <pre > {
+                         JSON.stringify(repos, null, 2)
+                     } </pre>
+                     }
             </React.Fragment>
         )
     }
+}
+
+
+
+
+NavBar.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguague: PropTypes.func.isRequired,
 }
